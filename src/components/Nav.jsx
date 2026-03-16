817be21f67e3946da47d1efa6abe3e2223e4d5ae
LAP0140\LAP0140 <hoangquy0417@gmail.com>
2026-03-16 14:24:30 +0700

    Create NavBar for portfolio

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,27 @@
+import dayjs from "dayjs";
+import {navIcons, navLinks} from "#constants";
+
 const Nav = () => {
     return (
-        <>
-            <h1>NavBar</h1>
-        </>
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo apple"/>
+                <p className="font-bold">Adrian's Portfolio</p>
+                <ul>
+                    {navLinks.map(({id, name}) => (<li key={id}><p>{name}</p></li>))}
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    {navIcons.map(({id, img}) => (
+                        <li key={id}>
+                            <img src={img} alt={`icon ${img}`} />
+                        </li>
+                    ))}
+                </ul>
+                <time>{dayjs().format("dd MMM D h:mm A")}</time>
+            </div>
+        </nav>
     )
 }
 
